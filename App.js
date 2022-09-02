8c692b495179355e228c88e3d2f7f71dcdca7685
viktorsbcs <electricjackiebeats@gmail.com>
2022-09-02 03:40:01 +0300

    tailwind bug fix

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
     <NavigationContainer>
       <TailwindProvider>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+          <Stack.Screen name="Home" component={HomeScreen}/>
         </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
-    // "./App.{js,jsx,ts,tsx}",
-    // "./src/**/*.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./screens/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
