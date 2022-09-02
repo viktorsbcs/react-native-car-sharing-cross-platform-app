8dab8aad85850e27b12ddc4fea3de87c73f9838e
viktorsbcs <electricjackiebeats@gmail.com>
2022-09-02 03:29:14 +0300

    React Navigation setup

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-green-500">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    // "./App.{js,jsx,ts,tsx}",
+    // "./src/**/*.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  // ...
 };
