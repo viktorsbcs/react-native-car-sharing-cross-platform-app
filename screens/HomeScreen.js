60c1016d20b0b6063c1b83f4445cb003c4246831
viktorsbcs <electricjackiebeats@gmail.com>
2022-09-05 04:22:09 +0300

    safe area view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text className="text-green-500">Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView className="top-10">
+      <Text className="text-green-500">
+        Open up App.js to start working on your app!
+      </Text>
+    </SafeAreaView>
   );
 };
 
