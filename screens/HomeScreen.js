8099809b7e160da70d5a26069c405b3b8f1d5c34
viktorsbcs <electricjackiebeats@gmail.com>
2022-09-06 02:55:52 +0300

    sample header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 
@@ -12,9 +12,21 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView className="top-10">
-      <Text className="text-green-500">
-        Open up App.js to start working on your app!
+    <SafeAreaView className="top-16">
+      <Text>
+        <View className="bg-emerald-500 flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 p-4 rounded-full bg-gray-300"
+          />
+
+          <View className="bg-amber-200">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-x">Current Location</Text>
+          </View>
+        </View>
       </Text>
     </SafeAreaView>
   );
