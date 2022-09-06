cc2f231e4f1f4e015a357838d1ff2e6f1cd12df5
viktorsbcs <electricjackiebeats@gmail.com>
2022-09-06 03:49:42 +0300

    adding icons to header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { SafeAreaView, Text, View, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,8 +14,7 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="top-16">
-      <Text>
-        <View className="bg-emerald-500 flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="bg-indigo-200 flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{
               uri: "https://links.papareact.com/wru",
@@ -24,10 +24,13 @@ const HomeScreen = () => {
 
           <View className="bg-amber-200">
             <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-x">Current Location</Text>
+            <Text className="font-bold text-xl">
+                Current Location
+                <ChevronDownIcon size={20} color="#00CCBB"/>
+            </Text>
           </View>
+          <UserIcon size={35} color="#00CCBB"/>
         </View>
-      </Text>
     </SafeAreaView>
   );
 };
